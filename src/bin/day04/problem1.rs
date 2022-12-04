26ae47c1e788369a320ba3f6f0a10a3135f31803
Matt Stegall <me@matthewstegall.com>
2022-12-04 08:33:28 -0500

    day 04

---
+++ src/bin/day04/problem1.rs
@@ -0,0 +1,26 @@
+use regex::Regex;
+
+pub fn main() {
+    let data = include_str!("./input.txt");
+
+    let format = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+
+    let result = data.lines().map(|l| {
+        let captures = format.captures(l).unwrap();
+        let l1 = captures[1].parse::<u32>().unwrap();
+        let h1 = captures[2].parse::<u32>().unwrap();
+        let l2 = captures[3].parse::<u32>().unwrap();
+        let h2 = captures[4].parse::<u32>().unwrap();
+
+
+        if l2 >= l1 && h2 <= h1 {
+             return 1
+        }
+
+        if l1 >= l2 && h1 <= h2 {
+            return 1
+       }
+        0
+    }).sum::<u32>();
+    println!("{:?}", result)
+}
