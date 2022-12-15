f7298800e207a0c3beef38edd9775ddbd25101d0
Matt Stegall <me@matthewstegall.com>
2022-12-15 08:33:34 -0500

    day 15

---
+++ src/bin/day15/problem2.rs
@@ -0,0 +1,67 @@
+use regex::Regex;
+
+pub fn main() {
+    let pattern =
+        Regex::new(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
+            .unwrap();
+
+    let data = include_str!("./input.txt")
+        .lines()
+        .map(|l| {
+            let c = pattern.captures(l).unwrap();
+            let sx = c[1].parse::<isize>().unwrap();
+            let sy = c[2].parse::<isize>().unwrap();
+            let bx = c[3].parse::<isize>().unwrap();
+            let by = c[4].parse::<isize>().unwrap();
+
+            let d = (sx - bx).abs() + (sy - by).abs();
+
+            ((sx, sy), (bx, by), d)
+        })
+        .collect::<Vec<_>>();
+
+    for i in 0..4000000 {
+        let mut ranges = data
+            .iter()
+            .filter_map(|(s, _, d)| {
+                let dy = (i - s.1).abs();
+                if dy < *d {
+                    return Some((s.0 - (d - dy), s.0 + (d - dy)));
+                }
+
+                return None;
+            })
+            .collect::<Vec<_>>();
+
+        ranges.sort();
+
+        let consolidated: Vec<(isize, isize)> = ranges.iter().fold(vec![], |mut acc, r| {
+            if let Some(last) = acc.pop() {
+                if last.1 + 1 >= r.0 {
+                    if r.1 > last.1 {
+                        acc.push((last.0, r.1));
+                    } else {
+                        acc.push((last.0, last.1));
+                    }
+                } else {
+                    acc.push(last);
+                    acc.push(*r);
+                }
+            } else {
+                acc.push(*r);
+            }
+
+            acc
+        });
+
+        if consolidated.len() > 1 {
+            println!(
+                "{}, {}, {}",
+                consolidated[0].1 + 1,
+                i,
+                (consolidated[0].1 + 1) * 4000000 + i
+            );
+            return;
+        }
+    }
+}
